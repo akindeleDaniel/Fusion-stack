f22ff5f78c767ce95f7488c34e64eadeaf9d679c
Ojo Daniel. A <ojodanielakindele@gmail.com>
2025-11-07 03:38:49 +0100

    Added error middleware and routes(profile and register),also ppracticed chaining

--- dist/memory.js
+++ dist/memory.js
@@ -7,6 +7,10 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(500).json({ error: err.message });
+});
 // logger
 function logger(req, res, next) {
     console.log(`${req.url} ${req.method}`);
@@ -19,6 +23,7 @@ function requestCounter(req, res, next) {
     console.log(`Number of request: ${requestCount}`);
     next();
 }
+// validation
 function validateUsers(req, res, next) {
     const { name, email } = req.body;
     if (!name || !email) {
@@ -26,10 +31,42 @@ function validateUsers(req, res, next) {
     }
     return next();
 }
+// authorization
+function authorization(req, res, next) {
+    const apiKey = req.headers["x-api-key"];
+    if (apiKey !== "12345") {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    return next();
+}
 app.use(logger);
 app.use(requestCounter);
+const users = [];
 app.get("/", (req, res) => {
     res.json({ message: "Hello!" });
+});
+app.get('/secret', authorization, (req, res) => {
+    res.json({ message: 'Secret unlocked' });
+});
+app.get('/profile', logger, authorization, (req, res) => {
+    res.json({ Name: 'Danny', Role: 'Admin' });
+});
+app.post('/register', (req, res) => {
+    const { firstName, lastName, email, DateOfBirth, password, gender } = req.body;
+    if (!firstName || !lastName || !email || !DateOfBirth || !password || !gender) {
+        res.status(400).json({ message: 'All areas reqiured' });
+        return;
+    }
+    const newUser = {
+        firstName,
+        lastName,
+        email,
+        DateOfBirth,
+        password,
+        gender
+    };
+    users.push(newUser);
+    res.status(200).json({ message: 'Registration sucessful', data: newUser });
 });
 app.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`);
--- src/memory.ts
+++ src/memory.ts
@@ -3,6 +3,11 @@ const app = express()
 const PORT = 3000
 app.use(express.json())     
 
+app.use((err:Error,req:Request,res:Response, next:NextFunction)=>{
+    console.log(err.message)
+    res.status(500).json({error:err.message})
+}) 
+
 // logger
 function logger (req:Request,res:Response, next: NextFunction){
     console.log(`${req.url} ${req.method}`)
@@ -25,14 +30,61 @@ function validateUsers(req: Request, res:Response,next:NextFunction){
     }
     return next()
 }
+// authorization
+function authorization(req:Request,res:Response,next:NextFunction){
+    const apiKey = req.headers["x-api-key"]
+    if (apiKey !== "12345"){
+        return res.status(401).json({error:"Unauthorized"})
+    }
+    return next()
+}
 
 
 app.use(logger)
 app.use(requestCounter)
 
+interface User  {
+    firstName:string,
+    lastName: string
+    email:string
+    DateOfBirth: string
+    password:string
+    gender:string
+}
+
+const users:User[] = []
+
 app.get("/", (req:Request, res:Response)=>{
     res.json({ message:"Hello!" })
 }) 
+
+app.get('/secret', authorization, (req:Request, res:Response) =>{
+    res.json({ message:'Secret unlocked'})
+})
+
+app.get('/profile',logger,authorization,(req:Request,res:Response)=>{
+    res.json({Name: 'Danny',Role: 'Admin'})
+})
+
+app.post('/register',(req:Request,res:Response)=>{
+    const {firstName,lastName,email,DateOfBirth,password,gender} = req.body
+
+    if (!firstName||!lastName||!email||!DateOfBirth||!password||!gender){
+       res.status(400).json({message:'All areas reqiured'})
+        return }
+
+        const newUser: User= {
+            firstName,
+            lastName,
+            email,
+            DateOfBirth,
+            password,
+            gender
+        }
+        users.push(newUser)
+
+        res.status(200).json({message:'Registration sucessful', data:newUser})
+})
 
 app.listen(PORT, () =>{
     console.log(`server running at http://localhost:${PORT}`)
